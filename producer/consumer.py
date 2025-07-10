1dfd608bf9894f0108c22436e0e12197c4b01860
Japan Gor <japan.gor@waisl.in>
2025-07-10 22:13:13 +0530

    Update consumer.py

--- producer/consumer.py
+++ producer/consumer.py
@@ -74,7 +74,7 @@ global_group_id = 'cricket-consumer-group'
 
 # --- PostgreSQL connection setup (adjust as needed) ---
 # Example: db_conn = psycopg2.connect(dbname='yourdb', user='youruser', password='yourpass', host='localhost')
-db_conn = psycopg2.connect(dbname='cricket_alarm', user='crickergmr', password='GMR@W@!$l',     host='103.209.90.18')
+db_conn = psycopg2.connect(dbname='cricket_alarm', user='cricketgmr', password='GMR@W@!$l',     host='103.209.90.18')
 
 # --- API: Register/Update User with FCM Token ---
 @app.route('/register', methods=['POST'])
